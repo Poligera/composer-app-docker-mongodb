b586708e2f2a825653c46829ef6411deb29c1f34
polinamoore <p_gerasimenko@yahoo.com>
2021-07-17 07:07:53 +0100

    All crud actions except update

--- client/script.js
+++ client/script.js
@@ -1,11 +1,13 @@
 const header = document.querySelector("header");
-const section = document.querySelector("section");
+const composerList = document.querySelector("#composer-list");
 const article = document.querySelector("article");
 const msgBtn = document.querySelector("#msg-button");
 const getAllComposersBtn = document.querySelector("#all-composers");
 const newComposerForm = document.querySelector("#new-composer-form");
 const getByNameForm = document.querySelector("#get-by-name");
 const getByCountryForm = document.querySelector("#get-by-country");
+const updateDiv = document.querySelector("#update-div");
+const updateForm = document.querySelector("#update-composer-form");
 
 // event listeners
 msgBtn.onclick = () => {
@@ -15,10 +17,8 @@ msgBtn.onclick = () => {
 };
 getAllComposersBtn.onclick = () => {
   getAllComposers();
-  getAllComposersBtn.style.backgroundColor = "gray";
-  getAllComposersBtn.style.textTransform = "uppercase";
-  getAllComposersBtn.disabled = true;
 };
+
 newComposerForm.addEventListener("submit", addNewComposer);
 getByNameForm.addEventListener("submit", getComposerByName);
 getByCountryForm.addEventListener("submit", getComposerByCountry);
@@ -41,12 +41,57 @@ function getAllComposers() {
     .then((data) => {
       data.composers.forEach((composer) => {
         const par = document.createElement("p");
+        const btnDiv = document.createElement("div");
+        const deleteBtn = document.createElement("button");
+        const updateBtn = document.createElement("button");
         par.textContent = `***\n${composer.fullName}\n${composer.country}\nborn: ${composer.birthYear}\ndied: ${composer.deathYear}`;
-        section.append(par);
+        updateBtn.textContent = "UPDATE";
+        deleteBtn.textContent = "DELETE";
+        btnDiv.append(updateBtn);
+        btnDiv.append(deleteBtn);
+        par.append(btnDiv);
+        composerList.append(par);
+        getAllComposersBtn.disabled = true;
+        deleteBtn.onclick = () => deleteComposer(composer.name, par);
+        updateBtn.onclick = () => {
+          updateDiv.style.display = "block";
+          updateForm.fullNameUpdate.value = composer.fullName;
+          updateForm.countryUpdate.value = composer.country;
+          updateForm.birthYearUpdate.value = composer.birthYear;
+          updateForm.deathYearUpdate.value = composer.deathYear;
+          updateForm.addEventListener("submit", (e) => {
+            e.preventDefault();
+            const updatedData = {
+              name: composer.name,
+              fullName: e.target.fullNameUpdate.value,
+              country: e.target.countryUpdate.value,
+              birthYear: parseInt(e.target.birthYearUpdate.value),
+              deathYear: parseInt(e.target.deathYearUpdate.value),
+            };
+            updateComposer(updatedData, updateDiv);
+          });
+        };
       });
     })
     .catch(console.warn);
 }
+
+// Update route:
+function updateComposer(data, el) {
+  const options = {
+    method: "PATCH",
+    body: JSON.stringify(data),
+    headers: { "Content-Type": "application/json" },
+  };
+
+  fetch(`http://localhost:3000/${data.name}`, options)
+    .then((data) => {
+      const { composer } = data;
+      el.textContent = `${data.name} has been updated`;
+    })
+    .catch(console.warn);
+}
+
 // composer by name route:
 function getComposerByName(e) {
   e.preventDefault();
@@ -98,13 +143,24 @@ function addNewComposer(e) {
     headers: { "Content-Type": "application/json" },
   };
 
-  fetch("http://localhost:3000/add", options)
+  fetch("http://localhost:3000/", options)
     .then((res) => res.json())
     .then((data) => {
       const par = document.createElement("p");
       par.textContent = `Composer added:\n${data.fullName}`;
       article.append(par);
+      composerList.textContent = "";
+      getAllComposersBtn.disabled = false;
     })
-    .then(() => e.target.reset())
+    .catch(console.warn);
+  e.target.reset();
+}
+
+function deleteComposer(name, el) {
+  const options = {
+    method: "DELETE",
+  };
+  fetch(`http://localhost:3000/${name}`, options)
+    .then((el.textContent = `${name} was deleted`))
     .catch(console.warn);
 }
